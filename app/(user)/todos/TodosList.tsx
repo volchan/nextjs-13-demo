6649e02721695e35f04a91823a1181aeff00661c
Stephane DE LA TORRE <s.dlt@hotmail.fr>
2022-11-10 18:52:34 +0100

    feat: React Suspense boundaries

--- app/(user)/todos/TodosList.tsx
+++ app/(user)/todos/TodosList.tsx
@@ -1,14 +1,20 @@
-import React from 'react'
-import Link from 'next/link'
+import React from "react";
+import Link from "next/link";
 
 const fetchTodos = async (): Promise<Todo[]> => {
-  const res = await fetch('https://jsonplaceholder.typicode.com/todos', { next: { revalidate: 60 } })
-  return res.json()
-}
+  // random timeout for testing purposes
+  const timeout = Math.floor(Math.random() * 5 + 1) * 1000;
+  await new Promise((resolve) => setTimeout(resolve, timeout));
+
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos", {
+    next: { revalidate: 60 },
+  });
+  return res.json();
+};
 
 async function TodosList() {
-  const todos = await fetchTodos()
-  
+  const todos = await fetchTodos();
+
   return (
     <>
       {todos.map((todo) => (
@@ -17,7 +23,7 @@ async function TodosList() {
         </p>
       ))}
     </>
-  )
+  );
 }
 
-export default TodosList
+export default TodosList;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { Suspense } from "react";
+import TodosList from "./(user)/todos/TodosList";
 
 function Home() {
   return (
-    <div className="text-red-500">I am the home page</div>
-  )
+    <div className="flex flex-col space-y-10">
+      <Suspense fallback={<p className="text-red-500">Loading the Todos...</p>}>
+        <div className="flex space-x-2">
+          {/* @ts-ignore */}
+          <TodosList />
+        </div>
+      </Suspense>
+
+      <Suspense
+        fallback={
+          <p className="text-blue-500">Loading the Shopping Trolleys...</p>
+        }
+      >
+        <div className="flex space-x-2">
+          {/* @ts-ignore */}
+          <TodosList />
+        </div>
+      </Suspense>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
